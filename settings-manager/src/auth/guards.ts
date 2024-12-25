97341412cd522b35483e3576f73cbada6785f78c
Unorec <152114265+Unorec@users.noreply.github.com>
2024-12-25 17:24:01 +0800

    Delete settings-manager directory

--- settings-manager/src/auth/guards.ts
+++
@@ -1,9 +0,0 @@
-<div id="admin-settings">
-    <h2>管理者專用設定區域</h2>
-    <ul>
-        <li><a href="manage_accounts.html">班別帳號密碼管理</a></li>
-        <li><a href="petty_cash.html">零用金設定</a></li>
-        <li><a href="backup.html">備份設定</a></li>
-        <li><a href="monthly_report.html">每月日報表設定</a></li>
-    </ul>
-</div>
